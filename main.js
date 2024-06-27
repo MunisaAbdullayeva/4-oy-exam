23424d7b2dbf85060e1ddd6ad749c745de9bcf47
MunisaAbdullayeva <abdullayevam385@gmail.com>
2024-06-27 09:19:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,160 @@
+// Examen JavaScript Amaliy savollar 
+
+// 1-savol Arifmetik operatorla
+
+//  5 sonini 2 ga bo'ling va qoldig`ini alertga chiqaring!
+
+// разделить 5 на 2 и вывести остаток в алерт (использовать оператор %)
+
+// javob:Pasdan yozib keting kodini
+
+alert(5 % 2)
+
+
+
+// 2-savol  Math metodlari
+
+// 1 dan 10 gacha bo'lgan random son yasang va alertga chiqaring
+
+// сделать рандомное число от 1 до 10 и вывести в алерт
+
+// Javob:Pasdan yozib keting kodini
+
+
+let a = Math.floor(Math.random() * 10);
+console.log(a)
+
+
+
+// 3-savol Math metodlari
+
+// 12.510 sonini butun songa aylantiring! 
+
+// сделать число 12.510 целым
+
+// ответ должен выйти 12
+
+// javob: 12 chiqishi kerak!
+
+let b = Math.floor(12.510)
+console.log(b)
+
+
+
+
+
+// 4-savol function
+
+// "MARS IT SCHOOL" sozini nechta harfdan iboratligini funksiya orqali topib va alertga chiqaring!.
+
+// с помощью функции найти сколько букв в слове "MARS IT SCHOOL" и вывести в алерт
+
+// Javob:Kodini yozib bering 
+
+let text = "MARS IT SCHOOL"
+console.log(text.length)
+
+
+
+// 5-savol for loop
+
+// "MARS IT SCHOOL" sozini javascript funksiyasi orqali 10 marta console.log ga chiqaring!
+
+// С помощью функции вывести в консоль 10 раз слово "MARS IT SCHOOL"
+
+// Javob:Kodini yozib bering
+
+
+for (let index = 0; index <= 10; index++) {
+    console.log("MARS IT SCHOOL")
+}
+
+
+
+// 6-savol Array methods
+
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+
+// Shu arraydan foydalangan holda console.log ga o'zingizni ismigizni chiqaring
+
+// С помощью этого массива вывести в консоль свое имя
+
+// Javob:Kodini yozib bering
+
+
+let harflar = ["a", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+
+console.log(harflar)
+
+console.log(harflar[11].toUpperCase() + harflar[19] + harflar[12] + harflar[7] + harflar[17] + harflar[0])
+
+
+
+
+
+
+// 7-savol if else
+
+// Promtdan yosh kiriting agar yoshi 18 yoshdan katta bo'lsa console da Siz balag'ot yoshiga yetgansiz , aks holda  siz balog'at yoshiga yetmagansiz  // agar yoshi 18 yoshga teng bolsa  balog'at yoshi muborak deb console da chiqarish kerak
+
+// Работа с if else, проверка возраста больше 18 лет, меньше 18 лет или равен 18 годам
+
+// Javob:Kodini yozib bering
+
+
+let son = prompt('yoshingizni yozing');
+
+if (son >= 18) {
+    let yosh = +prompt("Siz balag'ot yoshiga yetgansiz");
+    console.log(son)
+} else if (son < 18) {
+    let = prompt("balog'at yoshi muborak");
+
+}
+
+
+
+
+
+// 8-savol String metodlari vs Array metodlari
+
+// Promtdan ismigizni kiriting va console.log da ismigizni teskarisini chiqaring
+
+// Ввести в промпт свое имя и вывести в консоль его наоборот
+
+// Javob:Kodini yozib bering
+
+
+
+
+let ism = +prompt("Ismingizni yozing")
+let reversedName = ("");
+for (let i = ism.length - 1; i >= 0; i--){
+    reversedName +- ism[i];
+}
+console.log(" ", reversedName)
+
+
+
+
+
+
+// 10-savol Array metodlari
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// shu arraydagi juft sonlarni console ga chiqaring
+
+// найти и вывести четные массивы в консоль, с помощью методов
+
+
+// Javob :Kodini yozib bering
+
+
+let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let even = sonlar.filter(item => item % 2 === 0)
+let odd = sonlar.filter(item => item % 2 === 0)
+
+console.log("JUFT SONLAR: " + even)
+console.log("JUFT SONLAR: " + odd)
